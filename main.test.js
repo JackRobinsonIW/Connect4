065789cbdc1223cb1b670bc6488f1c3ce124765c
Jack <jack.robinson@infinityworks.com>
2020-09-07 17:17:06 +0100

    Logic Tests Added

--- main.test.js
+++ main.test.js
@@ -1,16 +1,178 @@
 const each = require('jest-each').default;
 const {
   columnEmptySpace, createEmptyBoardState, switchPlayer, updateScore, checkWinner,
-} = require('./main-test');
+} = require('./main');
 
-test('columnEmptySpace 0,0 empty', () => {
-  // columnEmptySpace
-  expected_output = 5;
+describe('createEmptyBoardState', () => {
+  each([
+    [
+      [6, 7],
+      [
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+      ],
+    ],
+    [
+      [1, 0],
+      [[]],
+    ],
+    [
+      [0, 0],
+      [],
+    ],
+  ]).it("when the input is '%s'", (input, expected) => {
+    expect(createEmptyBoardState(input[0], input[1])).toStrictEqual(expected);
+  });
+});
 
-  // Act
-  board = createEmptyBoardState(6, 7);
-  actual_output = columnEmptySpace(board, 0);
+describe('columnEmpty', () => {
+  each([
+    [
+      [0,
+        [
+          [null, null, null, null, null, null, null],
+          [null, null, null, null, null, null, null],
+          [null, null, null, null, null, null, null],
+          [null, null, null, null, null, null, null],
+          [null, null, null, null, null, null, null],
+          [null, null, null, null, null, null, null],
+        ],
+      ],
+      5,
+    ],
+    [
+      [1,
+        [
+          [null, null, null, null, null, null, null],
+          [null, null, null, null, null, null, null],
+          [null, null, null, null, null, null, null],
+          [null, null, null, null, null, null, null],
+          [null, null, null, null, null, null, null],
+          [null, 'yellow', null, null, null, null, null],
+        ],
+      ],
+      4,
+    ],
+    [
+      [2,
+        [
+          [null, null, 'yellow', null, null, null, null],
+          [null, null, 'yellow', null, null, null, null],
+          [null, null, 'yellow', null, null, null, null],
+          [null, null, 'yellow', null, null, null, null],
+          [null, null, 'yellow', null, null, null, null],
+          [null, null, 'yellow', null, null, null, null],
+        ],
+      ],
+      null,
+    ],
+  ]).it("when the input is '%s'", (input, expected) => {
+    expect(columnEmptySpace(input[0], input[1])).toStrictEqual(expected);
+  });
+});
 
-  // Assert
-  expect(actual_output).toBe(expected_output);
+describe('switchPlayer', () => {
+  each([
+    [
+      'yellow',
+      'red',
+    ],
+    [
+      'red',
+      'yellow',
+    ],
+  ]).it("when the input is '%s'", (input, expected) => {
+    expect(switchPlayer(input)).toStrictEqual(expected);
+  });
+});
+
+describe('updateScore', () => {
+  each([
+    [
+      ['yellow', [0, 0]],
+      [1, 0],
+    ],
+    [
+      ['red', [0, 0]],
+      [0, 1],
+    ],
+  ]).it("when the input is '%s'", (input, expected) => {
+    expect(updateScore(input[0], input[1])).toStrictEqual(expected);
+  });
+});
+
+describe('checkWinner', () => {
+  each([
+    [
+      [2, 2, 4,
+        [
+          [null, null, null, null, null, null, null],
+          [null, null, null, null, null, null, null],
+          [null, null, 'yellow', null, null, null, null],
+          [null, null, 'yellow', null, null, null, null],
+          [null, null, 'yellow', null, null, null, null],
+          [null, null, 'yellow', null, null, null, null],
+        ],
+      ],
+      [[2, 2], [3, 2], [4, 2], [5, 2]],
+    ],
+    [
+      [5, 1, 4,
+        [
+          [null, null, null, null, null, null, null],
+          [null, null, null, null, null, null, null],
+          [null, null, null, null, null, null, null],
+          [null, null, null, null, null, null, null],
+          [null, null, null, null, null, null, null],
+          ['yellow', 'yellow', 'yellow', 'yellow', null, null, null],
+        ],
+      ],
+      [[5, 1], [5, 0], [5, 2], [5, 3]],
+    ],
+    [
+      [2, 3, 5,
+        [
+          [null, null, null, null, null, null, null],
+          [null, null, null, null, 'yellow', null, null],
+          [null, null, null, 'yellow', null, null, null],
+          [null, null, 'yellow', null, null, null, null],
+          [null, 'yellow', null, null, null, null, null],
+          ['yellow', null, null, null, null, null, null],
+        ],
+      ],
+      [[2, 3], [3, 2], [4, 1], [5, 0], [1, 4]],
+    ],
+    [
+      [5, 2, 3,
+        [
+          [null, null, null, null, null, null, null],
+          [null, null, null, null, null, null, null],
+          [null, null, null, null, null, null, null],
+          ['yellow', null, null, null, null, null, null],
+          [null, 'yellow', null, null, null, null, null],
+          [null, null, 'yellow', null, null, null, null],
+        ],
+      ],
+      [[5, 2], [4, 1], [3, 0]],
+    ],
+    [
+      [4, 2, 4,
+        [
+          [null, null, null, null, null, null, null],
+          [null, null, null, null, null, null, null],
+          [null, null, null, null, null, null, null],
+          [null, 'yellow', 'yellow', 'yellow', null, null, null],
+          [null, 'yellow', 'yellow', 'yellow', null, null, null],
+          [null, 'yellow', 'yellow', 'yellow', null, null, null],
+        ],
+      ],
+      [[4, 2]],
+    ],
+  ]).it("when the input is '%s'", (input, expected) => {
+    expect(checkWinner(input[0], input[1], input[2], input[3])).toStrictEqual(expected);
+  });
 });

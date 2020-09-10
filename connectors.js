aff0a4c6149580e40cb1972d919a8e52ca8831d0
Jack <jack.robinson@infinityworks.com>
2020-09-10 15:52:13 +0100

    First full version with server side hosting

--- connectors.js
+++ connectors.js
@@ -1,29 +1,33 @@
 /* eslint-disable no-console */
-const winCounter = [0, 0];
-const player = 'yellow';
-const lengthNeeded = 4;
-let inputValid = true;
-let turnCount = 0;
-let board = [[null, null, null, null, null, null, null],
-  [null, null, null, null, null, null, null],
-  [null, null, null, null, null, null, null],
-  [null, null, null, null, null, null, null],
-  [null, null, null, null, null, null, null],
-  [null, null, null, null, null, null, null]];
-let gameState = {};
+
+
+let gameState = {
+  turnCount: 0,
+  player: 'yellow',
+  inputValid: true,
+  lengthNeeded: 4,
+  winCounter: [0, 0],
+  board: [[null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null]],
+  winningPoints: [],
+};
 
 function highlightOn(column) {
-  if (inputValid === true) {
-    const row = columnEmptySpace(column, board);
+  if (gameState.inputValid === true) {
+    const row = columnEmptySpace(column, gameState.board);
     if (row === null) {
       return;
     }
-    $(`#${row}_${column}`).css('backgroundColor', player);
+    $(`#${row}_${column}`).css('backgroundColor', gameState.player);
   }
 }
 
 function highlightOff(column) {
-  const row = columnEmptySpace(column, board);
+  const row = columnEmptySpace(column, gameState.board);
   if (row === null) {
     return;
   }
@@ -40,8 +44,8 @@ function sizeSquares() {
   const titleheight = $('#title').css('height');
   const height = window.innerHeight - parseFloat(titleheight);
   // Calculate new desired width and height
-  const newWidth = (parseFloat(width) / board[0].length) * 0.8;
-  const newHeight = (height / board.length) * 0.9;
+  const newWidth = (parseFloat(width) / gameState.board[0].length) * 0.8;
+  const newHeight = (height / gameState.board.length) * 0.9;
   // Take the scale as the minimum of width and height
   const scale = `${Math.min(newWidth, newHeight)}px`;
   // Apply the scale to the board squares
@@ -50,17 +54,17 @@ function sizeSquares() {
 
 function drawBoard() {
   // Loop over the array
-  for (let i = 0; i < board.length; i += 1) {
-    for (let j = 0; j < board[i].length; j += 1) {
+  for (let i = 0; i < gameState.board.length; i += 1) {
+    for (let j = 0; j < gameState.board[i].length; j += 1) {
       // If the array element is not null set the html colour to the string in that element
-      if (board[i][j]) {
-        $(`#${i}_${j}`).css('backgroundColor', board[i][j]);
+      if (gameState.board[i][j]) {
+        $(`#${i}_${j}`).css('backgroundColor', gameState.board[i][j]);
       }
     }
   }
   // Update scoreboard
-  $('#wins-yellow').text(winCounter[0]);
-  $('#wins-red').text(winCounter[1]);
+  $('#wins-yellow').text(gameState.winCounter[0]);
+  $('#wins-red').text(gameState.winCounter[1]);
 }
 
 function highlightWinner(winningPoints) {
@@ -71,62 +75,48 @@ function highlightWinner(winningPoints) {
   }
 }
 
-// function squareClicked(column) {
-//   if (inputValid === false) {
-//     return;
-//   }
-//   const j = column;
-//   const i = columnEmptySpace(column, board);
-//   if (i === null) {
-//     return;
-//   }
-//   board[i][j] = player;
-//   const winningPoints = checkWinner(i, j, lengthNeeded, board);
-//   drawBoard();
-//   if (winningPoints.length > 0) {
-//     winCounter = updateScore(player, winCounter);
-//     inputValid = false;
-//     drawBoard();
-//     highlightWinner(winningPoints);
-//   }
-//   turnCount += 1;
-//   if (turnCount === board.length * board[0].length) {
-//     displayDraw();
-//   }
-//   player = switchPlayer(player);
-//   highlightOn(column);
-// }
-
-function squareClicked(column) {
-  if (inputValid === false) {
-    return;
-  }
-  const j = column;
-  const i = columnEmptySpace(column, board);
-  if (i === null) {
-    return;
-  }
+function postTurn(player, i, j) {
   $.ajax({
     url: `http://localhost:8080/takeTurn/${player}/${i}/${j}`,
     type: 'POST',
     crossDomain: true,
-    success: console.log('success'),
-    error: console.log('error'),
+    success(data) {
+      gameState = data;
+      console.log(`${i}_${j} Filled clicked`);
+      console.log(gameState);
+      if (gameState.turnCount === gameState.board.length * gameState.board[0].length) {
+        displayDraw();
+      }
+      if (gameState.winningPoints.length > 0) {
+        highlightWinner(gameState.winningPoints);
+      }
+      highlightOn(j);
+    },
   });
 }
 
+function squareClicked(column) {
+  if (gameState.inputValid === false) {
+    return;
+  }
+  const j = column;
+  const i = columnEmptySpace(column, gameState.board);
+  if (i === null) {
+    return;
+  }
+  postTurn(gameState.player, i, j);
+}
+
 function generateGrid() {
-  for (let i = 0; i < board.length; i += 1) {
+  for (let i = 0; i < gameState.board.length; i += 1) {
     // Create row Divs in "gameboard"
     const rowDiv = $('<div></div>').attr('id', `row${i}`).addClass('row').addClass('board-row');
     // Fill row Div with squares
-    for (let j = 0; j < board[i].length; j += 1) {
+    for (let j = 0; j < gameState.board[i].length; j += 1) {
       const colDiv = $('<div></div>').attr('id', `${i}_${j}`)
         .addClass('board-square')
         .text('')
         .click(() => squareClicked(j))
-        // .mouseover(highlightOn.bind(null, j))
-        // .mouseout(highlightOff.bind(null, j));
         .mouseover(() => highlightOn(j))
         .mouseout(() => highlightOff(j));
       rowDiv.append(colDiv);
@@ -137,7 +127,6 @@ function generateGrid() {
 }
 
 function clearGrid() {
-  inputValid = true;
   $('#gameboard').empty();
   let rows = $('#rows-input').val();
   let cols = $('#cols-input').val();
@@ -147,77 +136,44 @@ function clearGrid() {
   if (!cols) {
     cols = 7;
   }
-  board = createEmptyBoardState(rows, cols);
-  turnCount = 0;
-  generateGrid();
-  drawBoard();
-  sizeSquares();
+  $.ajax({
+    url: `http://localhost:8080/initGameBoard/${rows}/${cols}`,
+    type: 'POST',
+    crossDomain: true,
+    success(data) {
+      gameState = data;
+      generateGrid();
+      drawBoard();
+      sizeSquares();
+      console.log('Clear clicked');
+      console.log(gameState);
+    },
+  });
+}
+
+function setLength() {
+  const userLength = $('#length-input').val();
+  $.ajax({
+    url: `http://localhost:8080/initGameLength/${userLength}`,
+    type: 'POST',
+    crossDomain: true,
+    success(data) {
+      gameState = data;
+      $('#connect-length').text(gameState.lengthNeeded);
+      console.log('Clear clicked');
+      console.log(gameState);
+    },
+  });
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  $('#clear').click(() => {
-    let userRows = $('#rows-input').val();
-    let userCols = $('#cols-input').val();
-    if (!userRows) {
-      userRows = 6;
-    }
-    if (!userCols) {
-      userCols = 7;
-    }
-    $.ajax({
-      url: `http://localhost:8080/initGameBoard/${userRows}/${userCols}`,
-      type: 'POST',
-      crossDomain: true,
-    });
-    clearGrid(); // Delete this once render written
-    console.log('Clear clicked');
-    // Needs to call render function once written
-  });
-
-  $('#length').click(() => {
-    $.ajax({
-      url: 'http://localhost:8080/gameState',
-      type: 'GET',
-      crossDomain: true,
-      success(data) {
-        gameState = data;
-        console.log('Length clicked');
-        console.log(gameState);
-      },
-    });
-    console.log('Length clicked');
-    // Needs to call render function once written
-  });
+  $('#length').click(() => setLength());
+  $('#clear').click(() => clearGrid());
+  generateGrid();
+  sizeSquares();
+  drawBoard();
 }, false);
 
 window.addEventListener('resize', () => {
-  sizeSquares(board);
+  sizeSquares(gameState.board);
 });
-
-generateGrid();
-sizeSquares();
-drawBoard();
-
-// $('#length').click(() => {
-//   const userLength = $('#length-input').val();
-//   // $('#connect-length').text(lengthNeeded); - Move to render function
-//   $.ajax({
-//     url: `http://localhost:8080/initGameLength/${userLength}`,
-//     type: 'POST',
-//     crossDomain: true,
-//   });
-//   console.log('Length clicked');
-//   // Needs to call render function once written
-// });
-
-// $('#length').click(() => {
-// $.ajax({
-//   url: 'http://localhost:8080/gameState',
-//   type: 'GET',
-//   crossDomain: true,
-//   success(data) {
-//     gameState = data;
-//     console.log('Length clicked');
-//     console.log(gameState);
-//   },
-// });
--- server.js
+++ server.js
@@ -1,69 +1,65 @@
+/* eslint-disable no-console */
 const express = require('express');
 const cors = require('cors');
-const { createEmptyBoardState } = require('./main');
+const {
+  createEmptyBoardState, switchPlayer, checkWinner, updateScore,
+} = require('./main');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const winCounter = [0, 0];
-const player = 'yellow';
-let lengthNeeded = 4;
-const inputValid = true;
-const turnCount = 0;
-let board = [[null, null, null, null, null, null, null],
-  [null, null, null, null, null, null, null],
-  [null, null, null, null, null, null, null],
-  [null, null, null, null, null, null, null],
-  [null, null, null, null, null, null, null],
-  [null, null, null, null, null, null, null]];
+const gameState = {
+  turnCount: 0,
+  player: 'yellow',
+  inputValid: true,
+  lengthNeeded: 4,
+  winCounter: [0, 0],
+  board: [[null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null]],
+  winningPoints: [],
+};
 
 app.listen(8080, () => {
   console.log('Server started 8080');
 });
 
 app.get('/gameState', (req, res) => {
-  res.send(
-    {
-      'scoreArray': winCounter,
-      'playerTurn': player,
-      'requiredLength': lengthNeeded,
-      'gameNotOver': inputValid,
-      'numberOfTurns': turnCount,
-      'boardArray': board,
-    },
-  );
+  res.send(gameState);
 });
 
 app.post('/initGameBoard/:rows/:cols', (req, res) => {
-  board = createEmptyBoardState(req.params.rows, req.params.cols);
-  res.send(
-    {
-      'boardArray': board,
-    },
-  );
+  gameState.board = createEmptyBoardState(req.params.rows, req.params.cols);
+  gameState.inputValid = true;
+  gameState.turnCount = 0;
+  gameState.winningPoints = [];
+  res.send(gameState);
 });
 
 app.post('/initGameLength/:desiredLength', (req, res) => {
-  lengthNeeded = parseInt(req.params.desiredLength, 10);
-  res.send(
-    {
-      'requiredLength': lengthNeeded,
-    },
-  );
+  gameState.lengthNeeded = parseInt(req.params.desiredLength, 10);
+  res.send(gameState);
 });
 
-app.post('/takeTurn/:playerString/:row/:col', (req, res) => {
-  board[parseInt(req.params.row, 10)][parseInt(req.params.col, 10)] = req.params.playerString;
-  res.send(
-    {
-      'scoreArray': winCounter,
-      'playerTurn': player,
-      'requiredLength': lengthNeeded,
-      'gameNotOver': inputValid,
-      'numberOfTurns': turnCount,
-      'boardArray': board,
-    },
-  );
+app.post('/takeTurn/:player/:row/:col', (req, res) => {
+  if (req.params.player === gameState.player && gameState.inputValid === true) {
+    const x = parseInt(req.params.row, 10);
+    const y = parseInt(req.params.col, 10);
+    if (gameState.board[x][y] === null) {
+      gameState.board[x][y] = req.params.player;
+      gameState.player = switchPlayer(gameState.player);
+      gameState.winningPoints = checkWinner(x, y, gameState.lengthNeeded, gameState.board);
+      if (gameState.winningPoints.length > 0) {
+        gameState.inputValid = false;
+        gameState.winCounter = updateScore(gameState.player, gameState.winCounter);
+      } else {
+        gameState.turnCount += 1;
+      }
+      res.send(gameState);
+    }
+  }
 });
-
